1cd37fc73f7c892f0b300c95eca90bfd3fc5a976
steviebps <stephenwodecki@gmail.com>
2020-06-30 19:55:09 -0500

    add JSON file to prerun

--- cmd/root.go
+++ cmd/root.go
@@ -1,22 +1,9 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -24,17 +11,45 @@ import (
 
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
+	rein "github.com/steviebps/rein/pkg"
 	utils "github.com/steviebps/rein/pkg/utils"
 )
 
 var cfgFile string
 var chamber string
+var c = rein.Chamber{Toggles: []rein.Toggle{}, Children: []rein.Chamber{}}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "rein",
 	Short: "Local and remote configuration management",
 	Long:  `CLI tool that helps with configuration management for local and remote JSON files`,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		chamberFile := viper.GetString("chamber")
+
+		if !utils.Exists(chamberFile) {
+			fmt.Printf("Could not find chamber file: \"%s\"\n", chamberFile)
+			os.Exit(1)
+		}
+
+		jsonFile, err := os.Open(chamberFile)
+		if err != nil {
+			fmt.Printf("Could not open chamber file: %s\n", chamberFile)
+			os.Exit(1)
+		}
+		defer jsonFile.Close()
+
+		byteValue, err := ioutil.ReadAll(jsonFile)
+		if err != nil {
+			fmt.Printf("Error reading chamber file: %s\n", err)
+			os.Exit(1)
+		}
+
+		if err := json.Unmarshal(byteValue, &c); err != nil {
+			fmt.Printf("Error reading JSON: %s\n", err)
+			os.Exit(1)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
