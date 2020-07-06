6bd3ee4e28201a889a7bbb913926f2698d0ef358
steviebps <stephenwodecki@gmail.com>
2020-07-05 20:22:09 -0500

    add SaveAndExit helper func

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,11 @@
 package utils
 
-import "os"
+import (
+	"fmt"
+	"os"
+
+	rein "github.com/steviebps/rein/pkg"
+)
 
 // FindExists returns the first file that exists
 func FindExists(names []string) string {
@@ -20,4 +25,22 @@ func Exists(name string) bool {
 		}
 	}
 	return true
+}
+
+// SaveAndExit Saves the chamber to the file specified
+func SaveAndExit(file string, c rein.Chamber) {
+	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	c.Print(f, true)
+	if err := f.Close(); err != nil {
+		fmt.Printf("Error closing file: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Save complete
+	os.Exit(0)
 }
