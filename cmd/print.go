59893da11cc117ea1965c0ce112b8470c7c956ac
steviebps <stephenwodecki@gmail.com>
2020-07-05 19:14:22 -0500

    set output of print cmd

--- cmd/print.go
+++ cmd/print.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -11,11 +14,29 @@ var printCmd = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		pretty, _ := cmd.Flags().GetBool("pretty")
-		c.Print(cmd.OutOrStdout(), pretty)
+		output, _ := cmd.Flags().GetString("output")
+
+		if output != "" {
+			f, err := os.OpenFile(output, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+
+			if err != nil {
+				fmt.Printf("Error opening file: %v\n", err)
+				os.Exit(1)
+			}
+			c.Print(f, pretty)
+			if err := f.Close(); err != nil {
+				fmt.Printf("Error closing file: %v\n", err)
+				os.Exit(1)
+			}
+		} else {
+			c.Print(cmd.OutOrStdout(), pretty)
+		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(printCmd)
 	printCmd.Flags().BoolP("pretty", "p", false, "Prints in pretty format")
+	printCmd.Flags().StringP("output", "o", "", "Sets the output file of the printed content")
 }
