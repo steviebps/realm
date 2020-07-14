3de35f7c7eac446dcd23e6507bd12c99c62c941b
steviebps <stephenwodecki@gmail.com>
2020-07-14 03:24:22 -0500

    wip build command

--- cmd/build.go
+++ cmd/build.go
@@ -2,26 +2,22 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 	rein "github.com/steviebps/rein/pkg"
+	"github.com/steviebps/rein/utils"
 )
 
 // buildCmd represents the build command
 var buildCmd = &cobra.Command{
 	Use:   "build",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Build chambers with inherited toggles.",
+	Long:  `TODO`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("build called")
-
 		compile(&globalChamber)
-		globalChamber.Print(cmd.OutOrStdout(), true)
+		globalChamber.WriteWith(cmd.OutOrStdout(), true)
+		os.Exit(0)
 	},
 }
 
@@ -29,20 +25,15 @@ func compile(parent *rein.Chamber) {
 	for i := range parent.Children {
 		built := parent.Children[i].InheritWith(parent.Toggles)
 		parent.Children[i].Toggles = built
+		fmt.Println(parent.Buildable || parent.App)
+		if parent.Buildable || parent.App {
+			file := "./" + parent.Name + ".json"
+			utils.WriteChamberToFile(file, *parent, true)
+		}
 		compile(parent.Children[i])
 	}
 }
 
 func init() {
 	rootCmd.AddCommand(buildCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// buildCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// buildCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
