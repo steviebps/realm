d0729d3c4332770aca35a4a55a93199ea99a9ecf
steviebps <stephenwodecki@gmail.com>
2020-07-24 22:19:55 -0500

    add basic type validation

--- pkg/toggle.go
+++ pkg/toggle.go
@@ -1,7 +1,26 @@
 package rein
 
+import (
+	"reflect"
+)
+
 type Toggle struct {
 	Name       string      `json:"name"`
 	ToggleType string      `json:"type"`
 	Value      interface{} `json:"value"`
+}
+
+func (t Toggle) IsValidType() bool {
+	typ := reflect.TypeOf(t.Value).String()
+
+	switch typ {
+	case "bool":
+		return t.ToggleType == "boolean"
+	case "string":
+		return t.ToggleType == "string"
+	case "float64":
+		return t.ToggleType == "number"
+	default:
+		return false
+	}
 }
