d9e5cb6dbadf3dd0303e39368506a9a466f96210
steviebps <stephenwodecki@gmail.com>
2020-08-30 15:26:02 -0500

    remove comments

--- options/stack.go
+++ options/stack.go
@@ -10,7 +10,7 @@ func (s *Stack) IsEmpty() bool {
 
 // Push add a new value onto the stack
 func (s *Stack) Push(option OpenOption) {
-	*s = append(*s, option) // Simply append the new value to the end of the stack
+	*s = append(*s, option)
 }
 
 // Pop Remove and return top element of stack. Return false if stack is empty.
@@ -18,9 +18,9 @@ func (s *Stack) Pop() (*OpenOption, bool) {
 	if s.IsEmpty() {
 		return nil, false
 	} else {
-		index := len(*s) - 1   // Get the index of the top most element.
-		element := (*s)[index] // Index into the slice and obtain the element.
-		*s = (*s)[:index]      // Remove it from the stack by slicing it off.
+		index := len(*s) - 1
+		element := (*s)[index]
+		*s = (*s)[:index]
 		return &element, true
 	}
 }
