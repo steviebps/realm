ebc61ee2d3575f5a56787d345d7d72d33d5e8be1
steviebps <stephenwodecki@gmail.com>
2020-08-30 13:56:55 -0500

    add comments

--- options/open.go
+++ options/open.go
@@ -4,8 +4,10 @@ import (
 	rein "github.com/steviebps/rein/pkg"
 )
 
+// SelectAction is the callback to be executed when the option has been selected
 type SelectAction func(*rein.Chamber)
 
+// OpenOption is a CLI option for a select command
 type OpenOption struct {
 	Name       string
 	Associated *rein.Chamber
@@ -13,21 +15,23 @@ type OpenOption struct {
 	Action     SelectAction
 }
 
+// New creates an "open" command option (OpenOption)
 func New(name string, associated *rein.Chamber, displayed *rein.Chamber, action SelectAction) OpenOption {
-	open := OpenOption{
+	option := OpenOption{
 		Name:       name,
 		Displayed:  displayed,
 		Associated: associated,
 		Action:     action,
 	}
 
-	if open.Displayed == nil {
-		open.Displayed = open.Associated
+	if option.Displayed == nil {
+		option.Displayed = option.Associated
 	}
 
-	return open
+	return option
 }
 
+// Run executes the Action with the Associated Chamber
 func (option OpenOption) Run() {
 	option.Action(option.Associated)
 }
