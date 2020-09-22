b701134006dac63df9688afe293c37157e9297a3
steviebps <stephenwodecki@gmail.com>
2020-09-21 20:13:27 -0500

    build by chamber name and output stdout

--- cmd/build.go
+++ cmd/build.go
@@ -11,29 +11,45 @@ import (
 	"github.com/steviebps/rein/utils"
 )
 
+var outputDir string
+var chamberName string
+var toStdout bool
+
 // buildCmd represents the build command
 var buildCmd = &cobra.Command{
 	Use:   "build",
 	Short: "Build chambers with inherited toggles.",
 	Long:  `Build command will take your chamber configs and compile them with their inherited values.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		outputDir, _ := cmd.Flags().GetString("outputDir")
+		var err error
+		outputDir, _ = cmd.Flags().GetString("output-dir")
+		chamberName, _ = cmd.Flags().GetString("chamber")
+		toStdout, _ = cmd.Flags().GetBool("to-stdout")
+
+		// defaults to working directory
+		if outputDir == "" {
+			outputDir, err = os.Getwd()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+		}
+
 		var wg sync.WaitGroup
-		compile(&globalChamber, outputDir, &wg)
+		compile(&globalChamber, &wg)
 		wg.Wait()
 		os.Exit(0)
 	},
 }
 
-func compile(parent *rein.Chamber, outputDir string, wg *sync.WaitGroup) {
-	if parent.Buildable || parent.App {
+func compile(parent *rein.Chamber, wg *sync.WaitGroup) {
+	searchingByName := chamberName != ""
+	foundByName := chamberName == parent.Name
+	if (searchingByName && foundByName) || (!searchingByName && (parent.Buildable || parent.App)) {
 		wg.Add(1)
 
-		// defaults to current directory
-		prefix := "./"
-		if outputDir != "" {
-			prefix, _ = filepath.Abs(outputDir)
-		}
+		prefix, _ := filepath.Abs(outputDir)
 
 		if _, err := os.Stat(prefix); os.IsNotExist(err) {
 			os.Mkdir(prefix, 0700)
@@ -42,19 +58,25 @@ func compile(parent *rein.Chamber, outputDir string, wg *sync.WaitGroup) {
 		file, _ := filepath.Abs(prefix + "/" + parent.Name + ".json")
 		go func() {
 			defer wg.Done()
-			utils.WriteInterfaceToFile(file, parent.Toggles, true)
-			fmt.Println(file)
+			if toStdout {
+				utils.WriteInterfaceWith(os.Stdout, parent.Toggles, true)
+			} else {
+				utils.WriteInterfaceToFile(file, parent.Toggles, true)
+				fmt.Println(file)
+			}
 		}()
 	}
 
 	for i := range parent.Children {
 		built := parent.Children[i].InheritWith(parent.Toggles)
 		parent.Children[i].Toggles = built
-		compile(parent.Children[i], outputDir, wg)
+		compile(parent.Children[i], wg)
 	}
 }
 
 func init() {
 	rootCmd.AddCommand(buildCmd)
-	buildCmd.Flags().StringP("outputDir", "o", "", "Sets the output directory of the built files")
+	buildCmd.Flags().StringP("output-dir", "o", "", "Sets the output directory of the built files.")
+	buildCmd.Flags().StringP("chamber", "c", "", "Builds the selected chamber only.")
+	buildCmd.Flags().Bool("to-stdout", false, "Prints the built files to stdout. (overrides output-dir flag)")
 }
