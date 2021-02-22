f1db49ae0175124e7d356549865cfc1aebf35b96
steviebps <stephenwodecki@gmail.com>
2021-02-21 21:07:44 -0600

    add color loggin

---
+++ internal/logger/logger.go
@@ -0,0 +1,25 @@
+package logger
+
+import (
+	"fmt"
+
+	"github.com/steviebps/rein/internal/colors"
+)
+
+func ErrorString(e string) {
+	fmt.Println(colors.Red(e))
+}
+
+func Error(e error) {
+	fmt.Println(colors.Red(e))
+}
+
+func ErrorWithPrefix(prefix string) func(string) {
+	return func(e string) {
+		ErrorString(prefix + e)
+	}
+}
+
+func InfoString(i string) {
+	fmt.Println(colors.Teal(i))
+}
