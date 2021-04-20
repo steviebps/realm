c62d02e2fe568120e64d4f860443c2ebdd36dd34
steviebps <stephenwodecki@gmail.com>
2021-04-19 23:31:12 -0500

    add chamber tests

--- pkg/chamber_test.go
+++ pkg/chamber_test.go
@@ -14,10 +14,14 @@ func TestFindByName(t *testing.T) {
 			bottom,
 		},
 	}
+	middle2 := &Chamber{
+		Name: "MIDDLE2",
+	}
 	top := &Chamber{
 		Name: "TOP",
 		Children: []*Chamber{
 			middle,
+			middle2,
 		},
 	}
 
@@ -27,6 +31,7 @@ func TestFindByName(t *testing.T) {
 	}{
 		{"BOTTOM", bottom},
 		{"MIDDLE", middle},
+		{"MIDDLE2", middle2},
 		{"TOP", top},
 	}
 
@@ -35,5 +40,47 @@ func TestFindByName(t *testing.T) {
 		if got != test.output {
 			t.Errorf("Got %v\nexpected: %q", got, test.output.Name)
 		}
+	}
+}
+
+func TestInheritWith(t *testing.T) {
+
+	bottom := &Chamber{
+		Name: "BOTTOM",
+		Toggles: map[string]*Toggle{
+			"toggle2": &Toggle{
+				Name:       "toggle1",
+				ToggleType: "boolean",
+				Value:      false,
+			},
+		},
+	}
+	top := &Chamber{
+		Name: "TOP",
+		Toggles: map[string]*Toggle{
+			"toggle1": &Toggle{
+				Name:       "toggle1",
+				ToggleType: "boolean",
+				Value:      false,
+			},
+		},
+	}
+	middle := &Chamber{
+		Name: "MIDDLE",
+		Toggles: map[string]*Toggle{
+			"toggle1": &Toggle{
+				Name:       "toggle1",
+				ToggleType: "boolean",
+				Value:      false,
+			},
+		},
+	}
+
+	middle.InheritWith(top.Toggles)
+	// should not inherit an already existent key
+	bottom.InheritWith(middle.Toggles)
+
+	if len(bottom.Toggles) != 2 {
+		t.Errorf("%q did not inherit properly from %q", bottom.Name, top.Name)
 	}
 }
