1830a4f3d8925270721d40972b30983daa0ad653
steviebps <stephenwodecki@gmail.com>
2021-05-21 21:51:09 -0500

    change to app-version flag

--- cmd/root.go
+++ cmd/root.go
@@ -56,8 +56,8 @@ func init() {
 
 	rootCmd.SetVersionTemplate(`{{printf "%s\n" .Version}}`)
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "rein configuration file")
-	rootCmd.PersistentFlags().StringP("version", "v", "", "runs all commands with a specified version")
-	viper.BindPFlag("version", rootCmd.PersistentFlags().Lookup("version"))
+	rootCmd.PersistentFlags().String("app-version", "", "runs all commands with a specified version")
+	viper.BindPFlag("app-version", rootCmd.PersistentFlags().Lookup("app-version"))
 }
 
 // initConfig reads in config file and ENV variables if set.
