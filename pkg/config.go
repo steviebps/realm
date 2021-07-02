254900236222008c642dfebae61212d8bdb08ac5
steviebps <stephenwodecki@gmail.com>
2021-07-01 20:30:25 -0500

    config wrapper
    Signed-off-by: steviebps <stephenwodecki@gmail.com>

--- pkg/config.go
+++ pkg/config.go
@@ -1,9 +1,14 @@
 package rein
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"os"
 	"path"
 
+	"github.com/steviebps/rein/internal/logger"
 	"golang.org/x/mod/semver"
 )
 
@@ -11,34 +16,129 @@ type Config struct {
 	rootChamber    Chamber
 	configPaths    []string
 	defaultVersion string
+	configFileUsed string
 }
 
 var config *Config
 
 func init() {
-	config = New()
+	config = newConfig()
 }
 
-func New() *Config {
+func newConfig() *Config {
 	return &Config{}
 }
 
-func SetDefaultVersion(version string) error {
+func SetVersion(version string) error { return config.SetVersion(version) }
+
+func (cfg *Config) SetVersion(version string) error {
 	if isValidVersion := semver.IsValid(version); !isValidVersion {
 		return fmt.Errorf("%q is not a valid semantic version", version)
 	}
 
-	config.defaultVersion = version
+	cfg.defaultVersion = version
 	return nil
 }
 
 func AddConfigPath(path string) error { return config.AddConfigPath(path) }
 
 func (cfg *Config) AddConfigPath(filePath string) error {
-	if path.Ext(filePath) != "json" {
+	if filePath == "" {
+		return errors.New("Config path cannot be empty")
+	}
+
+	if path.Ext(filePath) != ".json" {
 		return fmt.Errorf("%q is not an acceptable file extension. Please use JSON.", filePath)
 	}
 
-	config.configPaths = append(config.configPaths, filePath)
+	cfg.configPaths = append(config.configPaths, filePath)
 	return nil
+}
+
+func ReadInConfig() error { return config.ReadInConfig() }
+
+func (cfg *Config) ReadInConfig() error {
+	var rc io.ReadCloser
+	var err error
+
+	for _, fileName := range cfg.configPaths {
+		rc, err = retrieveLocalConfig(fileName)
+		if err != nil {
+			logger.ErrorString(fmt.Sprintf("Error reading file: %v", err))
+			continue
+		}
+		cfg.configFileUsed = fileName
+		break
+	}
+
+	if rc == nil {
+		return fmt.Errorf("Error reading file %q", cfg.configFileUsed)
+	}
+
+	byteValue, err := io.ReadAll(rc)
+	if err != nil {
+		wrappedErr := fmt.Errorf("Error reading file %q: %w", cfg.configFileUsed, err)
+		logger.ErrorString(wrappedErr.Error())
+		return wrappedErr
+	}
+
+	if err := json.Unmarshal(byteValue, &cfg.rootChamber); err != nil {
+		wrappedErr := fmt.Errorf("Error reading file %q: %w", cfg.configFileUsed, err)
+		logger.ErrorString(wrappedErr.Error())
+		return wrappedErr
+	}
+
+	return nil
+}
+
+func BoolValue(toggleKey string, defaultValue bool) bool {
+	return config.BoolValue(toggleKey, defaultValue)
+}
+
+func (cfg *Config) BoolValue(toggleKey string, defaultValue bool) bool {
+	cBool, ok := cfg.rootChamber.GetToggleValue(toggleKey, cfg.defaultVersion).(bool)
+	if !ok {
+		return defaultValue
+	}
+
+	return cBool
+}
+
+func StringValue(toggleKey string, defaultValue string) string {
+	return config.StringValue(toggleKey, defaultValue)
+}
+
+func (cfg *Config) StringValue(toggleKey string, defaultValue string) string {
+	cStr, ok := cfg.rootChamber.GetToggleValue(toggleKey, cfg.defaultVersion).(string)
+	if !ok {
+		return defaultValue
+	}
+
+	return cStr
+}
+
+func Float64Value(toggleKey string, defaultValue float64) float64 {
+	return config.Float64Value(toggleKey, defaultValue)
+}
+
+func (cfg *Config) Float64Value(toggleKey string, defaultValue float64) float64 {
+	cFloat64, ok := cfg.rootChamber.GetToggleValue(toggleKey, cfg.defaultVersion).(float64)
+	if !ok {
+		return defaultValue
+	}
+
+	return cFloat64
+}
+
+// func retrieveRemoteConfig(url string) (*http.Response, error) {
+// 	return http.Get(url)
+// }
+
+func retrieveLocalConfig(fileName string) (io.ReadCloser, error) {
+	file, err := os.OpenFile(fileName, os.O_RDONLY, 0755)
+	if errors.Is(err, os.ErrNotExist) {
+		return nil, fmt.Errorf("Could not open file %q: %w", fileName, err)
+	}
+
+	return file, nil
 }
