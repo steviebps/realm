7bbe769adf127eaba8d64d76bb1a879144406f31
steviebps <stephenwodecki@gmail.com>
2021-08-29 13:39:29 -0500

    cleanup utils

--- utils/utils.go
+++ utils/utils.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/url"
 	"os"
@@ -31,36 +30,6 @@ func Exists(name string) bool {
 		return true
 	}
 	return false
-}
-
-func openFileWriter(fileName string) (*bufio.Writer, *os.File) {
-	file, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
-	if err != nil {
-		fmt.Printf("Error opening file: %v\n", err)
-		os.Exit(1)
-	}
-	return bufio.NewWriter(file), file
-}
-
-func WriteInterfaceToFile(fileName string, i interface{}, pretty bool) error {
-	bw, file := openFileWriter(fileName)
-	enc := json.NewEncoder(bw)
-
-	if pretty {
-		enc.SetIndent("", "  ")
-	}
-
-	if err := enc.Encode(i); err != nil {
-		return err
-	}
-
-	bw.Flush()
-
-	if err := file.Close(); err != nil {
-		return err
-	}
-
-	return nil
 }
 
 func WriteInterfaceWith(w io.Writer, i interface{}, pretty bool) error {
