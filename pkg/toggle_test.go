0ab93a510f3dd2bbd70a1dd21ebaa72fabd9ed74
Stephen Wodecki <stephenwodecki@gmail.com>
2021-10-10 18:14:16 -0500

    write first toggle test

---
+++ pkg/toggle_test.go
@@ -0,0 +1,30 @@
+package realm
+
+import (
+	"testing"
+)
+
+func TestIsValidValue(t *testing.T) {
+
+	tests := []struct {
+		assertedType string
+		input        interface{}
+		output       bool
+	}{
+		{"boolean", false, true},
+		{"boolean", "string", false},
+		{"string", "string", true},
+		{"string", 0, false},
+		{"number", 1000.00, true},
+		{"number", false, false},
+	}
+	toggle := Toggle{}
+
+	for _, test := range tests {
+		toggle.ToggleType = test.assertedType
+		got := toggle.IsValidValue(test.input)
+		if got != test.output {
+			t.Errorf("input: %v with asserted type: %v\nreturned %v expected: %v", test.input, test.assertedType, got, test.output)
+		}
+	}
+}
