d5a66f9dccdd6c1e63a9d418b0a830c94a6f1a0a
Stephen Wodecki <stephenwodecki@gmail.com>
2025-11-11 20:30:50 -0500

    react compiler

--- http/realm-ui/vite.config.ts
+++ http/realm-ui/vite.config.ts
@@ -5,10 +5,20 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 import tailwindcss from "@tailwindcss/vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
+const ReactCompilerConfig = {
+  target: '18' // '17' | '18' | '19'
+};
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }): UserConfig => {
   return {
-    plugins: [basicSsl(), react(), tailwindcss(), flowbiteReact()],
+    plugins: [basicSsl(), react({
+      babel: {
+        plugins: [
+          ["babel-plugin-react-compiler", ReactCompilerConfig],
+        ],
+      },
+    }), tailwindcss(), flowbiteReact()],
     base: '/ui',
     build: {
       rollupOptions: {
